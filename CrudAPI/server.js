925d6e440a1512a348fdcb82bee5c97fc0ded86c
Awaiz <awaiz123nasir123@gmail.com>
2026-09-10 01:46:09 +0500

    Stage 2: read endpoints with 404

--- CrudAPI/server.js
+++ CrudAPI/server.js
@@ -10,6 +10,25 @@ app.get('/', (req, res) => {
   })
 });
 
+let tasks = [
+  { id: 1, title: 'Just Walking', done: false },
+  { id: 2, title: 'Driving', done: false },
+  { id: 3, title: 'Write documentation', done: false }
+];
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const task = tasks.find(task => task.id === id);
+  if (!task) {
+    return res.status(404).json({ error: `Task ${id} not available /found` });
+  }
+  res.json(task);
+});
+
 app.get('/health',(req,res) => {
   res.json({ status: 'ok'})
 });
