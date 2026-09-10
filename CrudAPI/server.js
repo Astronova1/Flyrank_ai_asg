54dc37117b8b41413ed9b62874c6943ff3f58e43
Astronova1 <awaiz123nasir123@gmail.com>
2026-09-10 20:14:05 +0500

    Create database

--- CrudAPI/server.js
+++ CrudAPI/server.js
@@ -1,8 +1,26 @@
+const Database = require('better-sqlite3');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const db = new Database('tasks.db')
+db.exec(`
+  CREATE TABLE IF NOT EXISTS tasks(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  done boolean NOT NULL
+  )
+  `)
+
+const dbcheck = db.prepare('SELECT COUNT(*) AS count FROM tasks').get()
+if (dbcheck.count ===0){
+  const insert = db.prepare('INSERT INTO tasks(title,done) VALUES (?,?)') 
+  insert.run("Shopping",0)
+  insert.run("Workout",0)
+  insert.run("study",0)
+}
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = {
@@ -76,12 +94,6 @@ app.get('/', (req, res) => {
     endpoints: ['/tasks']
   })
 });
-
-let tasks = [
-  { id: 1, title: 'Just Walking', done: false },
-  { id: 2, title: 'Driving', done: false },
-  { id: 3, title: 'Write documentation', done: false }
-];
 
 app.get('/tasks', (req, res) => {
   res.status(200).json(tasks);
