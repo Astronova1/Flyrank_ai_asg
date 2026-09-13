f6fb72b2793bd7be74ffd0237dc1be3ed3ccfd45
Astronova1 <awaiz123nasir123@gmail.com>
2026-09-13 16:13:26 +0500

    Create js file repo for postgre functions

---
+++ CrudAPI/postgrerepo.js
@@ -0,0 +1,31 @@
+const { Pool } = require('pg')
+const pool = new Pool ({connectionString: process.env.DATABASE_URL})
+
+module.exports={
+    async getAll(){
+        const {rows} = await pool.query('SELECT * FROM tasks ORDER by id');
+        return rows
+    },
+
+    async getById(id){
+        const {row} = await pool.query('SELECT * FROM tasks WHERE id= $1',[id])
+        return row[0] || null
+    },
+    
+    async create(title,done){
+        const {row} = await pool.query('INSERT INTO tasks(title,done) VALUES ($1, $2) RETURNING *', [title,done])
+        return[0]
+    },
+
+    async update(id,title,done){
+         const { rows } = await pool.query(
+      'UPDATE tasks SET title = $1, done = $2 WHERE id = $3 RETURNING *',
+      [title, done, id]
+    );
+    return rows[0] || null;
+  },
+
+  async remove(id) {
+    await pool.query('DELETE FROM tasks WHERE id = $1', [id]);
+    }
+}
