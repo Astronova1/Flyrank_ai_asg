59940cfe373e3b797f7fab6e8444b6f22791f01a
Astronova1 <awaiz123nasir123@gmail.com>
2026-09-14 16:57:46 +0500

    Stage 1: signup and login routes working

---
+++ BE03/routes/auth.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import supabase from '../lib/supabase.js'
+
+const router = express.Router()
+
+router.post('/signup', async(req, res) =>{
+    const {email, password} = req.body
+
+    if (!email || !password){
+        return res.status(400).json({error: "Please enter a valid email and password"})
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+        email, password,
+    })
+
+    if (error){
+        return res.status(401).json({error: error.message})
+    }
+
+    res.status(201).json({user: data.user})
+})
+
+router.post('/login', async(req,res)=>{
+    const {email, password} = req.body
+
+        if (!email || !password){
+        return res.status(400).json({error: "Please enter a valid email and password"})
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,password,
+    })
+
+    if (error){
+        return res.status(401).json({error: error.message})
+    }
+    res.status(200).json({
+            JWT_token: data.session.access_token,
+            refresh_token: data.session.refresh_token,
+            user: data.user
+        }
+    )
+})
+
+export default router
--- BE03/server.js
+++ BE03/server.js
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
 import supabase from './lib/supabase.js'
+import authRouter from './routes/auth.js'
 
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json());
+app.use('/auth', authRouter)
 
 
 app.listen(port, () => {
